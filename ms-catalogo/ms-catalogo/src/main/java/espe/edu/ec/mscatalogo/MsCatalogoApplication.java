3d30809e91835a27f28ea081bb0ae068fc1f9249
Mateo Ruben Colina Morales <mrcolina@espe.edu.ec>
2025-12-09 20:02:57 -0500

    Creación de proyecto SpringBoot para el ms de catalogo

---
+++ ms-catalogo/ms-catalogo/src/main/java/espe/edu/ec/mscatalogo/MsCatalogoApplication.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.mscatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCatalogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCatalogoApplication.class, args);
+    }
+
+}
