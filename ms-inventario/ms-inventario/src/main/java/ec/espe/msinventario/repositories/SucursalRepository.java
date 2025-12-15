39d6270a60a14ee4db5c4f8f6cf85afbdb5778a7
lacarrillo2001 <lacarrillo6@espe.edu.ec>
2025-12-15 08:40:23 -0500

    update

--- ms-inventario/ms-inventario/src/main/java/ec/espe/msinventario/repositories/SucursalRepository.java
+++
@@ -1,7 +0,0 @@
-package ec.espe.msinventario.repositories;
-
-import ec.espe.msinventario.models.entities.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
-}
