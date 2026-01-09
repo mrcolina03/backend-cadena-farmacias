e96aef6855dabca0b015a60d8d8ba5143abe9063
lacarrillo2001 <lacarrillo6@espe.edu.ec>
2026-01-09 17:33:16 -0500

    add services APIGATEWAY

---
+++ gateway/src/main/java/com/example/gateway/config/CorsConfig.java
@@ -0,0 +1,8 @@
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+    // La configuración de CORS se gestionará en application.yaml
+}
